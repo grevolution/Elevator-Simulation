4560a5449bd13e3f0c9961bc54134e3753b2c49f
Shan Ul Haq <shan.ulhaq@blooper.local>
2011-11-21 20:51:00 +0500

    Initial Commit

---
+++ ElevatorSimulation/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  ElevatorSimulation
+//
+//  Created by Shan Ul Haq on 11/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ElevatorSimulation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ElevatorSimulation
+//
+//  Created by Shan Ul Haq on 11/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
